4400ec144e463f1c61f9ea09288e9010b2e58fe2
JanySalinas <miss_jany48@hotmail.com>
2025-04-09 20:41:27 +0200

    Fix CSS import paths and other updates

--- config/database.js
+++ config/database.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('Brews_Blues', 'root', 'admin', {
-    host: 'localhost',
-    dialect: 'mysql'
-  });
-  
-  module.exports = sequelize;
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'Brews_Blues',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASS || 'admin',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
+  }
+);
+
+module.exports = sequelize;
   
